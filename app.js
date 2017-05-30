c32efd6ed92d755dd613a01faca6b7c672bc2894
anujashelke <anuja_shelke@outlook.com>
2017-05-30 15:38:33 +0530

    submitted

---
+++ app.js
@@ -0,0 +1,13 @@
+var Employee = {"name" : "Anuja", "age" : 25, "salary" : 25000, "address" : {"city" : "Pune", "state" : "Maharashtra" , "pincode" : 411038} };
+
+document.write("NAME :" + Employee.name + "<br />");
+
+document.write("DOB :" + Employee.age + "<br />");
+
+document.write("DOB :" + Employee.salary + "<br />");
+
+document.write("City :" + Employee.address.city + "<br />");
+
+document.write("State :" + Employee.address.state + "<br />");
+
+document.write("pincode :" + Employee.address.pincode + "<br />");
